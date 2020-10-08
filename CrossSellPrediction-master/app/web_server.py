860f4bd2a6279104a0f6e908dd59bb24b353b210
Luiz Felipe Soares <38364154+luizfsoares@users.noreply.github.com>
2020-10-08 14:47:37 -0300

    new:dev: updating_flask_code

--- CrossSellPrediction-master/app/web_server.py
+++ CrossSellPrediction-master/app/web_server.py
@@ -12,18 +12,32 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
 
-        print('antes')
-        data = request.form
-        print('depois')
+    print('antes')
+    data = request.form
+    print('depois')
 
-        model_input = utils.preprocess(data)
-        result = utils.predict(model_input)
+    model_input = utils.preprocess(data)
+    result = utils.predict(model_input)
 
     return render_template('index.html',
                            prediction_text='O preço é {}'.format(result))
 
+@app.route('/sobre')
+def sobre():
+
+    return render_template('sobre.html')
+
+@app.route('/contato')
+def contato():
+
+    return render_template('contato.html')
+
+@app.route('/index1')
+def home2():
+
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run(app.run(host='127.0.0.1', port=5015))
+    app.run(threaded=True, host="0.0.0.0", port=8080)
